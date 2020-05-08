8a67478c5bfc2748a8bf881c6808053118515458
Marcin Juranek <m.juranek@PGG.PL>
2020-05-08 12:30:20 +0200

    work, not good. i must have time

--- Assets/Balistic_Calculation.cs
+++ Assets/Balistic_Calculation.cs
@@ -6,18 +6,22 @@ public class Balistic_Calculation
     public float velocity { get; set; }
     public float angle;
 
-    public float radianAngle { get; private set; }
-    public float timeOfFly { get; private set; }
+    public float radianAngle { get; set; }
+    public float timeOfFly { get; set; }
     private float lengthpartOfTheDistance;
     private int distancePartNumber;
     private float g;
     private int resolution;
     private float maxDistance;
 
-
-    public Balistic_Calculation(int resolution, float velocity, float angle)
+    public Balistic_Calculation()
     {
         g = Mathf.Abs(Physics2D.gravity.y);
+        this.distancePartNumber = 1;
+    }
+    public Balistic_Calculation(int resolution, float velocity, float angle)
+        :this()
+    {
         if ((resolution == 0) || (resolution < 0))
         {
             this.resolution = 10;
@@ -26,67 +30,86 @@ public class Balistic_Calculation
         {
             this.resolution = resolution;
         }
-        this.distancePartNumber = 1;
         this.angle = angle;
         this.velocity = velocity;
     }
-    public Vector3 CalculateArcOneVector(int partOfDistance)
+    public Vector3 CalculateArcOneVector()
     {
-        if (partOfDistance <= this.distancePartNumber)
-        {
-            return Vector3.zero;
-        }
         Vector3 result;
+        if (distancePartNumber == this.resolution)
+        {
+            return new Vector3(0,0, maxDistance);
+        }
+         Debug.Log("distancePartNumber= " + distancePartNumber.ToString());
+        float partOfRoad = (float) distancePartNumber / resolution;
+
+        //convert angle to radina angle
         radianAngle = ConvertAngleToRadian(angle);
-        Debug.Log("Radian Angle= " + radianAngle.ToString());
+
+        //calculate the maximum distance
         maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
-        //get time for one part of maxdistance
-        timeOfFly = CalculateTimeOfFly(velocity, radianAngle, maxDistance) * (partOfDistance/resolution);
-        //get length part of distance
-        lengthpartOfTheDistance = maxDistance / resolution;
-        result = CalculateArcPoint(timeOfFly, lengthpartOfTheDistance);
+        Debug.Log("Distance= " + maxDistance.ToString());
+
+        //calculate time of fly in one part of road
+        timeOfFly = CalculateTimeOfFly(velocity, radianAngle, maxDistance, partOfRoad);
+        Debug.Log("Time of fly= " + timeOfFly.ToString());
+
+        //calculate y in one part of road
+        result = CalculateArcPoint(timeOfFly, maxDistance, partOfRoad);
         this.distancePartNumber++;
         return result;
     }
+    /// <summary>
+    /// test with time
+    /// </summary>
+    /// <param name="partOfDistance"></param>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    //public Vector3 CalculateArcOneVector(int partOfDistance, float time)
+    //{
+    //    Vector3 result;
+
+    //    radianAngle = ConvertAngleToRadian(angle);
+    //    //Debug.Log("Radian Angle= " + radianAngle.ToString());
+
+    //    maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
+    //    //Debug.Log("Distance= " + maxDistance.ToString());
+
+    //    timeOfFly = CalculateTimeOfFly(velocity, radianAngle, maxDistance) * (partOfDistance / resolution);
+    //    Debug.Log("Time of fly= " + timeOfFly.ToString());
+
+    //    //get length part of distance
+    //    lengthpartOfTheDistance = maxDistance / resolution;
+    //    Debug.Log("Length one part of distance= " + (maxDistance / resolution).ToString());
+
+    //    result = CalculateArcPoint(time, lengthpartOfTheDistance);
+    //    this.distancePartNumber++;
+    //    return result;
+    //}
     public float ConvertAngleToRadian(float angle)
     {
         return Mathf.Deg2Rad * angle;
     }
-    private Vector3[] CalculateArcArray()
-    {
-        Vector3[] arcArray = new Vector3[resolution + 1];
-
-        radianAngle = Mathf.Deg2Rad * angle;
-
-        //Debug.Log("radianAngle=" + radianAngle.ToString());
-        //Debug.Log("Velocity=" + throwLine.velocity.ToString());
-        //Debug.Log("Mathf.Sin(2 * radianAngle)=" + Mathf.Sin(2 * radianAngle).ToString());
-        //Debug.Log("throwLine.g=" + g.ToString());
-
-        float maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
-        //  Debug.Log("maxDistance=" + maxDistance.ToString());
-        for (int i = 0; i <= resolution; i++)
-        {
-            float t = (float)i / (float)resolution;
-            arcArray[i] = CalculateArcPoint(t, maxDistance);
-        }
-        return arcArray;
-    }
-    private Vector3 CalculateArcPoint(float t, float x)
+   
+    private Vector3 CalculateArcPoint(float t, float x, float amountDistance)
     {
         //s = V*t
-       // float x = t * maxDistance;
+       float s = t * x * amountDistance;
         
-        float y = x * Mathf.Tan(radianAngle) - ((g * x * x) /
+        float y = s * Mathf.Tan(radianAngle) - ((g * s * s) /
             (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
-        //  Debug.Log("Y=" + y.ToString());
-        return new Vector3(x, y, 0);
+         Debug.Log("droga=" + s.ToString());
+         Debug.Log("wysokosc=" + y.ToString());
+        return new Vector3(0, y, s);
     }
 
-    private float CalculateTimeOfFly(float velocity, float radianangle, float maxdistance)
+    private float CalculateTimeOfFly(float velocity, float radianangle, float maxdistance,float numberOfPartDistance)
     {
         float timefly;
-        timefly = maxdistance / velocity * Mathf.Cos(radianAngle);
+      //  Debug.Log("maxdistance in time method= " + maxdistance.ToString());
+        timefly = maxdistance / velocity * Mathf.Cos(radianangle) * numberOfPartDistance;
+        //Debug.Log("velocity * Mathf.Cos(radianAngle) = " + (velocity * Mathf.Cos(radianangle)).ToString());
+       // Debug.Log("timefly in method = " + timefly.ToString());
         return timefly;
     }
 }
--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -15,27 +15,43 @@ public class BallScript : MonoBehaviour
     private Rigidbody rb;
     private ThrowBall throwMethod = new ThrowBall();
     private Balistic_Calculation ballFly;
+
+    int framesToSkip;
     // Start is called before the first frame update
     private void Awake()
     {
         ballGameObject = GameObject.Find("Sphere");
         ballStartPosition = ballGameObject.transform.position;
-        ballFly = new Balistic_Calculation(10, 14f, 45f);
+        ballFly = new Balistic_Calculation(20, 20f, 45f);
+        framesToSkip = UnityEngine.Random.Range(0, 10);
     }
     void Start()
     {
+       
         rb = ballGameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.MovePosition(ballFly.CalculateArcOneVector(1));
-        for (int i = 0; i <= throwMethod.resolution; i++)
+        if (framesToSkip > 0)
         {
-            rb.MovePosition(throwMethod.CalculateArcArray()[i]);
+            framesToSkip--;
         }
-    }
+        else
+        {
+            rb.MovePosition(ballFly.CalculateArcOneVector());
+
+            Debug.Log("Position z=" + this.transform.position.z);
+            framesToSkip = 10;
+
+        }
+            // Debug.Log("Position y=" + this.transform.position.y);
+            //for (int i = 0; i <= throwMethod.resolution; i++)
+            //{
+            //    rb.MovePosition(throwMethod.CalculateArcArray()[i]);
+            //}
+        }
 
     
    
