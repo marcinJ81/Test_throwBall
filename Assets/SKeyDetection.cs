a8461faf4afa0546cb46de45bc585328de5dcb0d
Marcin Juranek <marcin.juranek2@gmail.com>
2020-06-21 12:34:26 +0200

    Add class for detections any keypresess

---
+++ Assets/SKeyDetection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+ 
+    public static class SKeyDetecion
+    {
+        /// <summary>
+        /// https://stackoverflow.com/questions/12076107/how-to-detect-if-any-key-is-pressed
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<KeyCode> KeysDown()
+        {
+            foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(key))
+                    yield return key;
+            }
+        }
+    }
+}
