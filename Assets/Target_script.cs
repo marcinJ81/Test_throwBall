28eac1379ed55d704e26a96d725ef7c9d00bea8d
Marcin Juranek <marcin.juranek2@gmail.com>
2020-06-21 13:11:26 +0200

    add two functions firt chnge angle value and second change velocity value

--- Assets/Target_script.cs
+++ Assets/Target_script.cs
@@ -17,86 +17,40 @@ public class Target_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //increase angle
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(0,4 * Time.deltaTime,0);
-            if (angle <= 89)
-                this.angle += 1;
-            Debug.Log("angle in script = " + angle.ToString());
-        }
-        //decrease angle
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(0,-4 * Time.deltaTime,0);
-            if (angle >= 1)
-                this.angle -= 1;
-            Debug.Log("angle in script = " + angle.ToString());
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (velocity >= 1)
-                this.velocity -= 1;
-            Debug.Log("velocity in script = " + velocity.ToString());
-        }
-         //increase velocity
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (velocity <= 100)
-                this.velocity += 1;
-            Debug.Log("velocity in script = " + velocity.ToString());
-        }
+       
     }
 
-    private float WhenKeyPressChangeValueAngle(KeyCode key)
+    void changeValue()
     {
+        //increase angle
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(0, 4 * Time.deltaTime, 0);
             if (angle <= 89)
-            {
                 this.angle += 1;
-                return angle;
-            }
+            Debug.Log("angle in script = " + angle.ToString());
         }
         //decrease angle
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(0, -4 * Time.deltaTime, 0);
             if (angle >= 1)
-            {
                 this.angle -= 1;
-                return angle;
-            }
+            Debug.Log("angle in script = " + angle.ToString());
         }
-        
-        return 0;
-           
-    }
-
-    private float WhenKeyPressChangeValueVelocity(KeyCode key)
-    {
-        //decrease velocity
         if (Input.GetKey(KeyCode.DownArrow))
         {
             if (velocity >= 1)
-            {
                 this.velocity -= 1;
-                return velocity;
-            }
+            Debug.Log("velocity in script = " + velocity.ToString());
         }
         //increase velocity
         if (Input.GetKey(KeyCode.UpArrow))
         {
             if (velocity <= 100)
-            {
                 this.velocity += 1;
-                return velocity;
-            }
+            Debug.Log("velocity in script = " + velocity.ToString());
         }
-        return 0;
-
     }
-
 
 }
